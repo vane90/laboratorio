a12b4b1aace53f9645df49e39176f1f4683fb9ed
vane90 <vane_slz@hotmail.com>
2013-07-01 22:27:45 -0500

    lab1

--- contraste.py
+++ contraste.py
@@ -9,22 +9,25 @@ from sys import argv # Importe para tabajar con argumentos
 def main():
     
     
-    foto1 = binarizar ()
+    foto1 = contraste ()
     
 
-def binarizar():
-  #toma imagen en escala de grises
-        image1 = Image.open("nueva.png")
+def contraste():
+    #toma imagen en escala de grises
+        image1 = Image.open("dory.jpg")
     	pixels = image1.load()
     	ancho,alto = image1.size
         minimo = int(argv[2]) #toma un valor umbral minimo
         for i in range(ancho):
             for j in range(alto):
-                if pixels[i,j][1] < minimo:
-                    p=0
-                else:
-                    p= 255
-                pixels[i,j]=(p,p,p)
+			a = pixels[i,j]
+			escala = (a[0] + a[1] + a[2])/3			
+			pixels[i,j] = (escala,escala, escala)
+                	if pixels[i,j][1] < minimo:
+                    		p=0
+                	else:
+                    		p= 255
+                	pixels[i,j]=(p,p,p)
         #fin = time()
         #tiempo_t = fin - inicio
         #print "Tiempo que tardo en ejecutarse binzarizar = "+str(tiempo_t)+" segundos"
