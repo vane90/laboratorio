f7a340127b7bd39390e9392f0b0d106b5f620e27
vane90 <vane_slz@hotmail.com>
2013-07-05 13:35:30 -0500

    lab5

--- lab5.py
+++ lab5.py
@@ -1,67 +1,34 @@
 from PIL import Image
-from sys import argv
 from math import floor
 import numpy as np
+from sys import argv
 
-def filtro(imagen_original):
-#    se encarga de tomar de cada pixel los pixeles 
-#    de izq, derecha, arriba, abajo y el mismo y los promedia, y ese
-#    promedio es el valor de los nuevos pixeles
-    
+
+
+def hacer_gris(imagen_original):
+    """pone la foto en escala de grises
+    toma el valor maximo del rgb de cada pixel
+    """
     x, y = imagen_original.size
-    imagen_difusa = Image.new("RGB", (x, y))
+    imagen_gris = Image.new("RGB", (x,y))
     pixeles = []
-    #temp sirve para obtener el promedio de los
-    #pixeles contiguos 
-    temp = []
     for a in range(y):
         for b in range(x):
-            actual = imagen_original.getpixel((b, a))[0]
-            if b>0 and b<(x-1) and a>0 and a<(y-1):
-                    #en esta condicion entran todos los pixeles que no estan
-                    #en el margen de la imagen, es decir casi todos
-                pix_izq = imagen_original.getpixel((b-1, a))[0]
-                pix_der = imagen_original.getpixel((b+1, a))[0]
-                pix_arriba = imagen_original.getpixel((b, a+1))[0]
-                pix_abajo = imagen_original.getpixel((b, a-1))[0]
-                temp.append(pix_izq)
-                temp.append(pix_der)
-                temp.append(pix_arriba)
-                temp.append(pix_abajo)
-            else:
-                #aqui entran todos los pixeles de la orilla
-                try:
-                    pix_abajo = imagen_original.getpixel((b, a-1))[0]
-                    temp.append(pix_abajo)
-                except:
-                    pass
-                try:
-                    pix_der = imagen_original.getpixel((b+1, a))[0]
-                    temp.append(pix_der)
-                except:
-                    pass
-                try:                
-                    pix_izq = imagen_original.getpixel((b-1, a))[0]
-                    temp.append(pix_izq)
-                except:
-                    pass
-                try:
-                    pix_arriba = imagen_original.getpixel((b, a+1))[0]
-                    temp.append(pix_arriba)
-                except:
-                    pass
-            temp.append(actual)
-                #se obtiene el promedio para cambiar el pixel
-            prom = sum(temp)/len(temp)
-            temp = []
-            pixeles.append((prom, prom, prom))
-    imagen_difusa.putdata(pixeles)
-    imagen_difusa.save("imagen_difusa.png")
-    return imagen_difusa
+            r, g, b = imagen_original.getpixel((b, a))
+            rgb = (r, g, b)
+                #se elige el valor mas grande
+            maximo = max(rgb)
+            data = (maximo, maximo, maximo)
+            pixeles.append(data)
+    imagen_gris.putdata(pixeles)
+    imagen_gris.save("imagen_gris.png")
+    return imagen_gris
+
 
 def aumenta_dism(antes, ((ancho, alto))):
-  prom_ancho = (x * 1.0)/ancho
+	prom_ancho = (x * 1.0)/ancho
 	prom_alto = (y * 1.0)/alto
+	imagen = np.zeros((alto,ancho,3))
 	
 	for a in range(alto):
 		for b in range(ancho):
@@ -70,21 +37,13 @@ def aumenta_dism(antes, ((ancho, alto))):
 			imagen[a,b] = antes[int(mueve_x),int(mueve_y)] 
 	return imagen
 
-
-
-def main():
-
-	#im = Image.open("dory.jpg").convert('RGB')
-	#(x,y) = im.size
-	#print 'Image size:', x,y
-	#imagen = np.array(im)
-	try:
-	    	imagen_inicial = "dory.jpg"
-	    	print imagen_inicial
-	    	imagen_original = Image.open(imagen_inicial)
-	    	imagen_original = imagen_original.convert('RGB')
-	except:
-        	print "selecciona la imagen"
-    		return	
-if __name__ == "__main__":
-    main()
+im = Image.open("dory.jpg").convert('RGB')
+(x,y) = im.size
+print 'Tamano original:', x,y
+imagen = np.array(im)	
+toma_ancho = int(argv[1])
+toma_alto = int(argv[2])
+imagen = aumenta_dism(imagen, (toma_ancho,toma_alto))
+print "Nuevo tamano: ", toma_ancho,toma_alto
+im = Image.fromarray(np.uint8(imagen))	
+im.save("redimencionada.png")
